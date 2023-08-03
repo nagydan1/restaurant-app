ebe03176d0a34af5551a7fa01c30777f211460e0
Daniel Nagy <daniel.nagy.w@gmail.com>
2023-08-03 20:54:54 +0100

    update mongoose connect

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,16 @@ import config from './config';
 const PORT = config.port || 8080;
 const URI = config.db.uri || 'mongodb://localhost:27017/restaurant';
 
-mongoose.connect(URI, (err) => {
-  if (err) logger.error(err.message);
-  logger.info('Successfully connected to MongoDB');
-});
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(URI);
+    logger.info('Successfully connected to MongoDB');
+  } catch (err) {
+    logger.error(err.message);
+  }
+};
+
+connectToMongo();
 
 app.listen(PORT, () => {
   logger.info(`App is listening on ${PORT}`);
