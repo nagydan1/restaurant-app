5a10aa113320067de27f2b1688d7ea3af8d599e4
Daniel Nagy <daniel.nagy.w@gmail.com>
2023-08-06 20:28:44 +0100

    api documentation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import { swaggerSettings } from './config';
+
 import logger from './logger';
 import apiRoutes from './api/api-routes';
 import errorHandler from './middlewares/error-handler';
 
 const app = express();
+const specs = swaggerJsDoc(swaggerSettings);
 
 app.use(morgan('combined', { stream: logger.stream }));
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use('/api', apiRoutes);
 
--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,22 @@
-export default {
+import packageJson from '../package.json';
+
+const config =  {
   port: process.env.PORT,
   db: {
     uri: process.env.DB_URI,
   },
 };
+
+export const swaggerSettings = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Restaurant API',
+      version: packageJson.version,
+    },
+    servers: [{ url: `http://localhost:${config.port}` }],
+  },
+  apis: ['**/*.yaml'],
+};
+
+export default config;
